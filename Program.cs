593ab1e110b5d051d0735b1bdff3654f9a7708cf
lilgenRock <33835993+lilgenRock@users.noreply.github.com>
2017-11-20 15:56:50 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Golf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //git test
+            Game game = new Game();
+            Hole hole = new Hole();
+            User user = new User();
+            while (game.Continues())
+            {
+                game.handler(game, user, hole);
+            }
+        }
+    }
+}
